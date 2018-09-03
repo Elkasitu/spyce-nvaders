0ba81c6b3e77630552979005f4ba032420424c99
Adrian Torres <beowolfmon_1@hotmail.com>
2018-09-03 18:53:01 +0200

    add some stack opcodes

--- cpu.py
+++ cpu.py
@@ -1,7 +1,7 @@
 from collections import namedtuple
 
 Flags = namedtuple("Flags", "z s p cy ac pad")
-State = namedtuple("State", "a b c d e h l sp pc mem cc int_enable")
+State = namedtuple("State", "a b c d e h l sp pc memory cc int_enable")
 
 
 def emulate(state):
@@ -9,7 +9,7 @@ def emulate(state):
     def parity(n):
         pass
 
-    opcode = state.mem[state.pc]
+    opcode = state.memory[state.pc]
 
     if opcode[0] == 0x01:
         state.c = opcode[1]
@@ -64,6 +64,10 @@ def emulate(state):
         state.cc.cy = ans > 0xff
         state.cc.p = parity(ans & 0xff)
         state.a = ans & 0xff
+    elif opcode[0] == 0xc1:     # POP B
+        state.c = state.memory[state.sp]
+        state.b = state.memory[state.sp + 1]
+        state.sp += 2
     elif opcode[0] == 0xc2:     # JNZ adr
         if state.cc.z == 0:
             state.pc = (opcode[2] << 8) | opcode[1]
@@ -71,6 +75,10 @@ def emulate(state):
             state.pc += 2
     elif opcode[0] == 0xc3:     # JMP adr
         state.pc = (opcode[2] << 8) | opcode[1]
+    elif opcode[0] == 0xc5:     # PUSH B
+        state.memory[state.sp - 1] = state.b
+        state.memory[state.sp - 2] = state.c
+        state.sp -= 2
     elif opcode[0] == 0xc6:     # ADI byte
         ans = int(state.a) + int(opcode[1])
         state.cc.z = ((ans & 0xff) == 0)
@@ -101,6 +109,25 @@ def emulate(state):
         state.cc.p = parity(x, 8)
         state.a = x
         state.pc += 1
+    elif opcode[0] == 0xf1:     # POP PSW
+        state.a = state.memory[state.sp + 1]
+        psw = state.memory[state.sp]
+        # copy each meaningful bit from psw to state
+        state.cc.z = (0x01 == (psw & 0x01))
+        state.cc.s = (0x02 == (psw & 0x02))
+        state.cc.p = (0x04 == (psw & 0x04))
+        state.cc.cy = (0x08 == (psw & 0x08))
+        state.cc.ac = (0x10 == (psw & 0x10))
+        state.sp += 2
+    elif opcode[0] == 0xf5:     # PUSH PSW
+        state.memory[state.sp - 1] = state.a
+        psw = state.cc.z
+        psw |= state.cc.s << 1
+        psw |= state.cc.p << 2
+        psw |= state.cc.cy << 3
+        psw |= state.cc.ac << 4
+        state.memory[state.sp - 2] = psw
+        state.sp -= 2
     elif opcode[0] == 0xfe:     # CPI byte
         x = state.a - opcode[1]
         state.cc.z = x == 0
