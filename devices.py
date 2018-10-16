a0030407d68c7dc571146bb70125ee92e141f282
Adrian Torres <adt@odoo.com>
2018-10-16 21:35:52 +0200

    fix timing issue

--- devices.py
+++ devices.py
@@ -8,17 +8,17 @@ class ShiftRegister:
         return (self._register >> self._offset) & 0xff
 
     def shift(self, val):
-        self._register = (self._register >> 8) | (val << 7)
+        self._register = (self._register >> 8) | (val << 8)
 
     def set_offset(self, val):
-        self._offset = (val ^ 0xff) & 0x07
+        self._offset = val & 0x08
 
 
 class Controller:
 
     def __init__(self):
-        self._p1_reg = 0x08
-        self._p2_reg = 0x00
+        self._p1_reg = 0b00001100
+        self._p2_reg = 0b00000001
 
     def get_p1(self):
         return self._p1_reg
@@ -39,7 +39,7 @@ class Controller:
 class Display:
 
     def __init__(self):
-        self.max_cycles = 100000/60
+        self.max_cycles = 2000000/60
 
     def refresh(self, cycles):
         if cycles >= self.max_cycles:
